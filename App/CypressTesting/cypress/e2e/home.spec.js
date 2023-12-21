5462d6b1f638d9ec7ab22481c40696bb1c5a301a
Owllark <igrbaran@gmail.com>
2023-12-22 01:55:37 +0300

    Changing structure of application for deployment
    Adding aspnetcoreapp directory with source code of application and Dockerfile

--- App/CypressTesting/cypress/e2e/home.spec.js
+++
@@ -1,6 +0,0 @@
-describe('template spec', () => {
-  it('home page should contain Welcome h1', () => {
-    cy.visit('https://localhost:44361/Home')
-    cy.get('h1').should('contain', 'Welcome')
-  })
-})
