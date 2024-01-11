ad4a5935c80db2bad4453e46c7d657e923fa9f09
Ihar Baran <70322536+Owllark@users.noreply.github.com>
2024-01-12 02:12:51 +0300

    Update cypress test

--- app/test/cypresstest/cypress/e2e/home.cy.js
+++ app/test/cypresstest/cypress/e2e/home.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('home page should contain Welcome h1', () => {
-    cy.visit('http://localhost:8080/Home')
+    cy.visit('http://localhost:8080')
     cy.get('h1').should('contain', 'Welcome')
   })
 })
