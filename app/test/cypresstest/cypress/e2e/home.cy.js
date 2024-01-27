74650641fdfd8c89922aa05bf922154c6b42889a
Ihar Baran <70322536+Owllark@users.noreply.github.com>
2024-01-27 05:33:51 +0300

    Update e2e test

--- app/test/cypresstest/cypress/e2e/home.cy.js
+++ app/test/cypresstest/cypress/e2e/home.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('home page should contain Welcome h1', () => {
-    cy.visit('http://webapp-service.staging.svc.cluster.local')
+    cy.visit('http://app-service.staging.svc.cluster.local')
     cy.get('h1').should('contain', 'Welcome')
   })
 })
