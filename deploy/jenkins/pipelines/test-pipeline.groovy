bb919072a9adfbf3baa84118ab1489416ad15cff
Owllark <igrbaran@gmail.com>
2024-02-05 06:55:17 +0300

    Update test-pipeline

--- deploy/jenkins/pipelines/test-pipeline.groovy
+++ deploy/jenkins/pipelines/test-pipeline.groovy
@@ -1,140 +1,118 @@
 
 pipeline {
-    environment {
-        registry = 'owllark/webapp'
-        registryCredential = 'dockerhub-owllark'
-        testsPassed = true
+    agent {
+        kubernetes {
+            inheritFrom "default"
+        }
     }
-    agent any
     stages{
-       stage('Testing') {
-            parallel {
-                stage('Unit tests') {
-                  agent {
-                    kubernetes {
-                        inheritFrom "backend-test-agent"
-                    }
-                }
-                    steps {
-                        script {
-                            container("backend-test") {
-                                sh """
-                                    cp /reports/test-results.trx ${WORKSPACE}
-                                    cp /reports/test_output ${WORKSPACE}
-                                """
-                                def cypressOutput = sh(script: 'cat test_output', returnStdout: true).trim()
-                                if (cypressOutput.contains("Passed!")) {
-                                        echo "All tests passed."
-                                    } else {
-                                        echo "Some tests failed. Setting build status to UNSTABLE."
-                                        currentBuild.result = 'UNSTABLE'
-                                        testsPassed = false
-                                    }
-                                sh """
-                                    rm -r /reports/*
-                                """
-                            }
-                        }
-                    }
-                    post {
-                        success {
-                            mstest testResultsFile:"test-results.trx", keepLongStdio: true
-                        }
-                        unstable {
-                            mstest testResultsFile:"test-results.trx", keepLongStdio: true
+       stage ('Clone staging branch') {
+           steps {
+               git credentialsId: 'github-owllark', url: 'git@github.com:Owllark/igorbaran_devops_internship_practice.git', branch: 'staging'
+           }
+       }
+
+        stage('Unit tests') {
+          agent {
+            kubernetes {
+                inheritFrom "backend-test-agent"
+            }
+        }
+            steps {
+                script {
+                    container("backend-test") {
+
+                        catchError(buildResult: 'SUCCESS', stageResult: 'SUCCESS') {
+                            sh """
+                                dotnet test /testing/test/app.unittest --results-directory /testing/TestResults --logger "trx;LogFileName=test-results.trx"
+                            """
                         }
                     }
                 }
+                script {
+                    container("backend-test") {
+                        sh """
+                            ls -la ${WORKSPACE}
+                            cp /testing/TestResults/test-results.trx ${WORKSPACE}
+                        """
+                    } 
+                }
+                
+            }
+            post {
+                always {
+                    mstest testResultsFile:"test-results.trx", keepLongStdio: true
+                }
+                
+            }
+        }
         
-                stage('E2E tests') {
-                  agent {
-                    kubernetes {
-                        inheritFrom "frontend-test-agent"
-                    }
-                  }
-                    steps {
-                        script {
-                              container("frontend-test") {
-                              sh """
-                                cp -r /reports ${WORKSPACE}
-                              """
-                              def cypressOutput = sh(script: 'cat reports/test_output', returnStdout: true).trim()
-                              if (cypressOutput.contains("All specs passed")) {
-                                    echo "All tests passed."
-                                } else {
-                                    echo "Some tests failed. Setting build status to UNSTABLE."
-                                    currentBuild.result = 'UNSTABLE'
-                                    testsPassed = false
-                                }
-                             sh """
-                                rm -r /reports/*
-                             """
-                            }
+        stage('E2E tests') {
+          agent {
+            kubernetes {
+                inheritFrom "frontend-test-agent"
+            }
+          }
+            steps {
+               script {
+                    container("frontend-test") {
+
+                        catchError(buildResult: 'SUCCESS', stageResult: 'SUCCESS') {
+                            sh """
+                                cd /testing
+                                npx cypress run --browser chromium
+                            """
                         }
                     }
-                    post {
-                        success {
-                             publishHTML([allowMissing: false,
-                                          alwaysLinkToLastBuild: false,
-                                          keepAll: true,
-                                          reportDir: 'reports',
-                                          reportFiles: 'report.html',
-                                          reportName: 'Cypress Report',
-                                          reportTitles: 'Cypress e2e tests',
-                                          useWrapperFileDirectly: true])
-                        }
-                        unstable {
-                             publishHTML([allowMissing: false,
-                                          alwaysLinkToLastBuild: false,
-                                          keepAll: true,
-                                          reportDir: 'reports',
-                                          reportFiles: 'report.html',
-                                          reportName: 'Cypress Report',
-                                          reportTitles: 'Cypress e2e tests',
-                                          useWrapperFileDirectly: true])
-                        }
-                    }
+                }
+                script {
+                    container("frontend-test") {
+                        sh """
+                            cp -r /testing/reports ${WORKSPACE}
+                            cp /testing/test-result.xml ${WORKSPACE}
+                        """
+                    } 
+                }
+            }
+            post {
+                always {
+                     publishHTML([allowMissing: false,
+                                  alwaysLinkToLastBuild: false,
+                                  keepAll: true,
+                                  reportDir: 'reports',
+                                  reportFiles: 'report.html',
+                                  reportName: 'Cypress Report',
+                                  reportTitles: 'Cypress e2e tests',
+                                  useWrapperFileDirectly: true])
+                     junit stdioRetention: '', testResults: 'test-result.xml'
                 }
             }
         }
+        
         stage('Push changes to release branch') {
-            agent {
-                kubernetes {
-                    inheritFrom "build-agent"
-                }
+            when {
+                expression { currentBuild.resultIsBetterOrEqualTo('SUCCESS') }
             }
                 steps {
-                    container("build") {
-                        script {
-                            if (testsPassed) {
-                                git credentialsId: 'github-owllark', url: 'git@github.com:Owllark/igorbaran_devops_internship_practice.git', branch: 'staging'
-
-                                def newImage = "${registry}:$BUILD_NUMBER"
-                                def deploymentFilePath = 'deploy/dev/deployment.yaml'
-                                
-                                withCredentials([string(credentialsId: 'GITHUB_HOST_KEY', variable: 'GITHUB_HOST_KEY')]) {
-                                    sh 'mkdir -p ~/.ssh && echo "$GITHUB_HOST_KEY" >> ~/.ssh/known_hosts'
-                                }
-
-                                sh """
-                                    git config --global user.email "jenkins@gmail.com"
-                                    git config --global user.name "Jenkins"
-                                    git config --global --add safe.directory /home/jenkins/agent/workspace/ci_pipeline
-                                """
-                                sh """
-                                    git checkout -b release
-                                    git merge staging
-                                """
-                                sshagent (credentials: ['github-owllark']) {
-                                    sh """
-                                        git push -f origin -- release
-                                    """
-                                }
-                            } else {
-                                echo "Cannot push - some tests failed"
-                            }
-                            
+                    script {
                         
+                        withCredentials([string(credentialsId: 'GITHUB_HOST_KEY', variable: 'GITHUB_HOST_KEY')]) {
+                            sh 'mkdir -p ~/.ssh && echo "$GITHUB_HOST_KEY" >> ~/.ssh/known_hosts'
+                        }
+    
+                        sh """
+                            git config --global user.email "jenkins@gmail.com"
+                            git config --global user.name "Jenkins"
+                            git config --global --add safe.directory /home/jenkins/agent/workspace/ci_pipeline
+                        """
+                        sh """
+                            git checkout -b release
+                            git merge staging
+                        """
+                        sshagent (credentials: ['github-owllark']) {
+                            sh """
+                                git push -f origin -- release
+                            """
                         }
                     }
                 }
