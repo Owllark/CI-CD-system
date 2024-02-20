62b8a6219d9d93a5fd49729d455c94bab011b461
owllark <owllark@debian.debain.org>
2024-02-20 07:28:36 +0300

    Update Makefile and ingress-controller installation

--- deploy/preinstall/ingress-controller.sh
+++ deploy/preinstall/ingress-controller.sh
@@ -1,6 +1,13 @@
+handle_error() {
+  echo "Error occurred in command: $1"
+  
+  exit 1
+}
+trap 'handle_error $BASH_COMMAND' ERR
+
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm upgrade --install ingress-controller ingress-nginx/ingress-nginx --namespace ingress-controller --create-namespace
-kubectl wait --namespace ingress-controller --for=condition=available deployment/ingress-controller-ingress-nginx-controller --timeout=300s
+kubectl wait --namespace ingress-controller --for=condition=Ready pod -l app.kubernetes.io/instance=ingress-controller --timeout=-1s
 
 LB_IP=$(kubectl get service ingress-controller-ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}' --namespace ingress-controller)
 
